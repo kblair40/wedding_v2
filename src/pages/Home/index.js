1f7eb20e7ec46a882f659f33c4903a8d18f995e0
Kevin Blair <kblair40@gmail.com>
2022-03-17 19:44:50 -0500

    Setup absolute imports

---
+++ src/pages/Home/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Text } from "@chakra-ui/react";
+
+const Home = () => {
+  return (
+    <Box>
+      <Text>HOME PAGE</Text>
+    </Box>
+  );
+};
+
+export default Home;
