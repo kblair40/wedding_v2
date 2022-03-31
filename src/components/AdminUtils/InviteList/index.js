5be77f01b402baf51ad664963a5cea22c5d1baa5
Kevin Blair <kblair40@gmail.com>
2022-03-31 08:47:51 -0400

    Add function in InviteList for uploading each guest

--- src/components/AdminUtils/InviteList/index.js
+++ src/components/AdminUtils/InviteList/index.js
@@ -14,6 +14,7 @@ import {
   useDisclosure,
 } from "@chakra-ui/react";
 
+import { addGuest } from "api/api";
 import ManageGuestModal from "./ManageGuestModal";
 
 const InviteList = ({ data }) => {
@@ -38,8 +39,10 @@ const InviteList = ({ data }) => {
     return data.slice(1).map((row) => {
       return (
         <Tr>
-          {row.map((text) => (
-            <Td>{text}</Td>
+          {row.map((text, i) => (
+            <Td textAlign={i > 0 ? "center" : undefined} key={i}>
+              {text}
+            </Td>
           ))}
           <Td>
             <Button
@@ -56,8 +59,62 @@ const InviteList = ({ data }) => {
     });
   };
 
+  const uploadGuests = async () => {
+    //
+    // addGuest();
+    for (let row of data) {
+      let guestData = {
+        first_name: row[0].split(" ")[0],
+        last_name: row[0].split(" ")[1],
+        priority: row[1],
+        att_exp: row[2],
+        replied: row[3],
+        dinner_selection: row[4],
+        age_range: row[5],
+        special_requests: row[6],
+        plus_one: row[7],
+        response: row[8],
+        email: row[9],
+        phone_number: row[10],
+        side: row[11],
+      };
+      // console.log("row:", row);
+      console.log("\nGUEST DATA:", guestData);
+      // let [first_name, last_name] = row[0].split(' ')
+      // guestData.first_name = first_name;
+      // guestData.last_name = last_name;
+
+      // guestData.priority = row[1]
+      // guestData.att_exp = row[2]
+      // guestData.replied = row[3]
+      // guestData.dinner_selection = row[4]
+      // guestData.age_range = row[5]
+      // guestData.
+    }
+  };
+
+  // {
+  //   first_name,
+  //   last_name,
+  //   priority,
+  //   att_exp,
+  //   replied,
+  //   dinner_selection,
+  //   age_range,
+  //   special_requests,
+  //   plus_one,
+  //   reponse,
+  //   email,
+  //   phone_number,
+  //   side,
+  //   timestamp: Timestamp.now(),
+  // }
+
   return (
     <React.Fragment>
+      <Button onClick={uploadGuests} mt="1rem" w="min-content">
+        Upload Guests
+      </Button>
       <TableContainer mt="24px">
         <Table size="sm" variant="striped">
           <Thead>{getHeader()}</Thead>
