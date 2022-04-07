3ac48d09cc26577927ee668d577c9089198b6999
Kevin Blair <kblair40@gmail.com>
2022-04-07 08:31:33 -0500

    Move NAV_ITEMS to constants file

--- src/components/NavSidebar/index.js
+++ src/components/NavSidebar/index.js
@@ -8,41 +8,29 @@ import {
   Collapse,
   Icon,
   Link,
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
   useColorModeValue,
-  useBreakpointValue,
   useDisclosure,
 } from "@chakra-ui/react";
-import {
-  HamburgerIcon,
-  CloseIcon,
-  ChevronDownIcon,
-  ChevronRightIcon,
-} from "@chakra-ui/icons";
+import { HamburgerIcon, CloseIcon, ChevronDownIcon } from "@chakra-ui/icons";
 
-export default function WithSubnavigation() {
+import { NAV_ITEMS } from "utils/constants";
+import NavLink from "components/NavLink";
+
+const Nav = () => {
   const { isOpen, onToggle } = useDisclosure();
 
   return (
-    <Box>
+    <Box mt="8px">
       <Flex
         bg={useColorModeValue("white", "gray.800")}
         color={useColorModeValue("gray.600", "white")}
         minH={"60px"}
-        py={{ base: 2 }}
-        px={{ base: 4 }}
-        borderBottom={1}
-        borderStyle={"solid"}
-        borderColor={useColorModeValue("gray.200", "gray.900")}
+        p="8px 16px"
+        border="1px solid #ccc"
+        justify="center"
         align={"center"}
       >
-        <Flex
-          flex={{ base: 1, md: "auto" }}
-          ml={{ base: -2 }}
-          display={{ base: "flex", md: "none" }}
-        >
+        <Flex border="1px solid red" display={{ base: "flex", md: "none" }}>
           <IconButton
             onClick={onToggle}
             icon={
@@ -52,8 +40,9 @@ export default function WithSubnavigation() {
             aria-label={"Toggle Navigation"}
           />
         </Flex>
-        <Flex flex={{ base: 1 }} justify={{ base: "center", md: "start" }}>
-          <Flex display={{ base: "none", md: "flex" }} ml={10}>
+
+        <Flex flex={1} justify="center" border="1px solid blue">
+          <Flex display={{ base: "none", md: "flex" }} justifyContent="center">
             <DesktopNav />
           </Flex>
         </Flex>
@@ -64,87 +53,19 @@ export default function WithSubnavigation() {
       </Collapse>
     </Box>
   );
-}
+};
+
+export default Nav;
 
 const DesktopNav = () => {
   return (
-    <Stack direction={"row"} spacing={4}>
+    <Stack direction={"row"} spacing="16px">
       {NAV_ITEMS.map((navItem) => (
         <Box key={navItem.label}>
-          <Popover trigger={"hover"} placement={"bottom-start"}>
-            <PopoverTrigger>
-              <Link
-                p={2}
-                href={navItem.href ?? "#"}
-                fontSize={"sm"}
-                fontWeight={500}
-                color="text.primary"
-                _hover={{
-                  textDecoration: "none",
-                  color: "text.secondary",
-                }}
-              >
-                {navItem.label}
-              </Link>
-            </PopoverTrigger>
-
-            {navItem.children && (
-              <PopoverContent
-                border={0}
-                boxShadow={"xl"}
-                bg={"neutral.white"}
-                p={4}
-                rounded={"xl"}
-                minW={"sm"}
-              >
-                <Stack>
-                  {navItem.children.map((child) => (
-                    <DesktopSubNav key={child.label} {...child} />
-                  ))}
-                </Stack>
-              </PopoverContent>
-            )}
-          </Popover>
+          <NavLink to={navItem.href}>{navItem.label}</NavLink>
         </Box>
       ))}
     </Stack>
-  );
-};
-
-const DesktopSubNav = ({ label, href, subLabel }) => {
-  return (
-    <Link
-      href={href}
-      role={"group"}
-      display={"block"}
-      p={2}
-      rounded={"md"}
-      _hover={{ bg: useColorModeValue("pink.50", "gray.900") }}
-    >
-      <Stack direction={"row"} align={"center"}>
-        <Box>
-          <Text
-            transition={"all .3s ease"}
-            _groupHover={{ color: "pink.400" }}
-            fontWeight={500}
-          >
-            {label}
-          </Text>
-          <Text fontSize={"sm"}>{subLabel}</Text>
-        </Box>
-        <Flex
-          transition={"all .3s ease"}
-          transform={"translateX(-10px)"}
-          opacity={0}
-          _groupHover={{ opacity: "100%", transform: "translateX(0)" }}
-          justify={"flex-end"}
-          align={"center"}
-          flex={1}
-        >
-          <Icon color={"pink.400"} w={5} h={5} as={ChevronRightIcon} />
-        </Flex>
-      </Stack>
-    </Link>
   );
 };
 
@@ -193,60 +114,6 @@ const MobileNavItem = ({ label, children, href }) => {
           />
         )}
       </Flex>
-
-      {/* <Collapse in={isOpen} animateOpacity style={{ marginTop: "0!important" }}>
-        <Stack
-          mt={2}
-          pl={4}
-          // borderLeft={1}
-          // borderStyle={"solid"}
-          // borderColor={useColorModeValue("gray.200", "gray.700")}
-          // borderColor="#f00"
-          align={"start"}
-        >
-          {children &&
-            children.map((child) => (
-              <Link key={child.label} py={2} href={child.href}>
-                {child.label}
-              </Link>
-            ))}
-        </Stack>
-      </Collapse> */}
     </Stack>
   );
 };
-
-const NAV_ITEMS = [
-  {
-    label: "Home",
-    href: "/",
-  },
-  {
-    label: "Travel",
-    href: "/travel",
-  },
-  {
-    label: "To Do/To Eat",
-    href: "/thingstodo",
-  },
-  {
-    label: "Registry",
-    href: "/registry",
-  },
-  {
-    label: "Wedding Party",
-    href: "/weddingparty",
-  },
-  {
-    label: "Gallery",
-    href: "/gallery",
-  },
-  {
-    label: "RSVP",
-    href: "/rsvp",
-  },
-  {
-    label: "Admin Stuff",
-    href: "/admin",
-  },
-];
---
+++ src/utils/constants.js
@@ -0,0 +1,34 @@
+export const NAV_ITEMS = [
+  {
+    label: "Home",
+    href: "/",
+  },
+  {
+    label: "Travel",
+    href: "/travel",
+  },
+  {
+    label: "To Do/To Eat",
+    href: "/thingstodo",
+  },
+  {
+    label: "Registry",
+    href: "/registry",
+  },
+  {
+    label: "Wedding Party",
+    href: "/weddingparty",
+  },
+  {
+    label: "Gallery",
+    href: "/gallery",
+  },
+  {
+    label: "RSVP",
+    href: "/rsvp",
+  },
+  {
+    label: "Admin Stuff",
+    href: "/admin",
+  },
+];
