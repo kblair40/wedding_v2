25a0bacd9f6cd07f1d6bf2140da395c961ce3355
Kevin Blair <kblair40@gmail.com>
2022-04-10 13:07:39 -0500

    Add skeleton for Gallery page

---
+++ src/pages/Gallery/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Box } from "@chakra-ui/react";
+
+import PageContainer from "components/containers/PageContainer";
+
+const Gallery = () => {
+  return <PageContainer center></PageContainer>;
+};
+
+export default Gallery;
