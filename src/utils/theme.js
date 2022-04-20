9cdac043ddaa37a623b1e532b53ba7d76a0c9a57
Kevin Blair <kblair40@gmail.com>
2022-04-20 08:05:48 -0500

    Finish adding full color palette to theme

--- src/utils/theme.js
+++ src/utils/theme.js
@@ -2,27 +2,73 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   colors: {
+    primary: {
+      main: "#F8EEF1", // current
+      50: "#FFFFFF",
+      100: "#fefdfe",
+      200: "#fefcfc",
+      300: "#fdfafb",
+      400: "#fcf8f9",
+      500: "#F8EEF1",
+      600: "#eaccd5",
+      700: "#dca9b8",
+      800: "#cd879c",
+      900: "#bf6480",
+    },
+    secondary: {
+      main: "#EC9A98", // pink
+      50: "#FFFFFF",
+      100: "#fdf5f5",
+      200: "#fbebea",
+      300: "#f9e1e0",
+      400: "#f7d7d6",
+      500: "#EC9A98",
+      600: "#e67a77",
+      700: "#e05a56",
+      800: "#da3a36",
+      900: "#c52824",
+    },
     neutral: {
-      black: "#2D2D2D",
+      black: "#344148",
+      main: "#344148",
+      white: "#f8eef1",
+      50: "#e9edef",
+      100: "#d2dadf",
+      200: "#bcc8ce",
+      300: "#a5b6be",
+      400: "#8fa3ae",
+      500: "#79919e",
+      600: "#657e8b",
+      700: "#546975",
+      800: "#44555e",
+      900: "#344148",
+    },
+    error: {
+      main: "#B50100",
+      50: "#ffffff",
+      100: "#ffdede",
+      200: "#ffbebd",
+      300: "#ff9d9c",
+      400: "#ff7c7b",
+      500: "#B50100",
+      600: "#a30100",
+      700: "#910100",
+      800: "#7f0100",
+      900: "#6d0100",
+    },
 
-      white: "#FAFAFA",
-      50: "#FAFAFA",
-      100: "#F7F5F0",
-      200: "#F0F0F0",
-      300: "#EEEEEE",
-    },
     text: {
-      primary: "rgba(45, 45, 45, 1)",
-      secondary: "rgba(45, 45, 45, .73)",
-      tertiary: "rgba(45, 45, 45, .51)",
-      muted: "rgba(45, 45, 45, .38)",
+      primary: "rgba(52, 65, 72, 1)",
+      secondary: "rgba(52, 65, 72, .78)",
+      tertiary: "rgba(52, 65, 72, .51)",
+      muted: "rgba(52, 65, 72 .38)",
     },
-    creme: {
-      main: "#DEBFB8",
-    },
-    gold: {
-      main: "#DCCAAA",
-    },
+    // creme: {
+    //   main: "#DEBFB8",
+    // },
+    // gold: {
+    //   main: "#DCCAAA",
+    // },
     // colorScheme for use with Radio and Checkbox components
     darkScheme: {
       50: "#999",
