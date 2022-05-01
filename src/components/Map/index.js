677fe904b950d09f5ceee184d9bb2549f67bb048
Kevin Blair <kblair40@gmail.com>
2022-05-01 15:34:50 -0500

    Update todos / remove unused imports

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { MapContainer } from "react-leaflet";
 import { Box } from "@chakra-ui/react";
-import { MickeyIcon } from "utils/icons";
 
 import MapContents from "./MapContents";
 
@@ -14,10 +13,6 @@ Add dropdown for requesting directions.
 const Map = () => {
   return (
     <Box>
-      {/* <Box border="1px solid #ccc">
-        <MickeyIcon />
-      </Box> */}
-
       <MapContainer
         maxBounds={[
           [28.2, -81.8],
