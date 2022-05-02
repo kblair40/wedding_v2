a1745459d2fffc7133aed54fbd12453a18f59ecf
Kevin Blair <kblair40@gmail.com>
2022-05-02 06:32:21 -0500

    Fix gallery images not aligning to center

--- src/components/GalleryImageCarousel/index.js
+++ src/components/GalleryImageCarousel/index.js
@@ -1,20 +1,11 @@
 import React, { useState } from "react";
-import {
-  Box,
-  IconButton,
-  useBreakpointValue,
-  Image,
-  Flex,
-} from "@chakra-ui/react";
+import { Box, IconButton, Image, Flex } from "@chakra-ui/react";
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 import Slider from "react-slick";
 
 export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
   // reference variable to change the state of custom buttons
   const [slider, setSlider] = useState(null);
-
-  const top = useBreakpointValue({ base: "90%", md: "50%" });
-  const side = useBreakpointValue({ base: "30%", md: "10px" });
 
   // Slider settings
   const settings = {
@@ -25,13 +16,13 @@ export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
     initialSlide: startingSlideIdx,
     centerMode: true,
     centerPadding: "16px",
+    className: "slick-slider",
+    adaptiveHeight: true,
   };
 
   const arrowBtnStyles = {
-    // mx: "auto",
     borderRadius: "full",
     position: "absolute",
-    // top: top,
     top: "50%",
     zIndex: 2,
     transform: "translate(0%, -50%)",
@@ -39,15 +30,14 @@ export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
 
   return (
     <Box
-      position={"relative"}
-      height="86vh"
-      width={"full"}
-      overflow={"hidden"}
+      position="relative"
+      height="90vh"
+      width="100%"
+      overflow="hidden"
       pt="12px"
     >
       <IconButton
         aria-label="left-arrow"
-        // left={side}
         left={{ base: "8px", sm: "16px" }}
         onClick={() => (slider ? slider.slickPrev() : undefined)}
         {...arrowBtnStyles}
@@ -57,7 +47,6 @@ export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
 
       <IconButton
         aria-label="right-arrow"
-        // right={side}
         right={{ base: "8px", sm: "16px" }}
         onClick={() => slider?.slickNext()}
         {...arrowBtnStyles}
@@ -65,41 +54,20 @@ export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
         <BiRightArrowAlt fontSize="24px" />
       </IconButton>
 
-      {/* <Flex alignItems="center"> */}
-      <Slider
-        {...settings}
-        ref={(slider) => setSlider(slider)}
-        style={
-          {
-            // display: "flex",
-            // alignItems: "center",
-            // border: "1px solid green",
-          }
-        }
-      >
+      <Slider ref={(slider) => setSlider(slider)} {...settings}>
         {imagesArray.map((url, index) => (
-          // <Flex
-          //   justifyContent="center"
-          //   w="100%"
-          //   alignItems="center"
-          //   // h="100%"
-          //   h="auto"
-          //   border="1px solid red"
-          // >
-          <Image
-            // mx="auto"
-            // my="auto"
-            src={url}
-            key={index}
-            // h="auto"
-            // maxH="80vh"
-            // maxW="100%"
-            borderRadius="4px"
-          />
-          // </Flex>
+          <Flex justifyContent="center" w="100%" alignItems="center" h="100%">
+            <Image
+              mx="auto"
+              src={url}
+              key={index}
+              maxH="80vh"
+              maxW="100%"
+              borderRadius="4px"
+            />
+          </Flex>
         ))}
       </Slider>
-      {/* </Flex> */}
     </Box>
   );
 };
