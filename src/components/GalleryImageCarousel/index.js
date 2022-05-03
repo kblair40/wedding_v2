dbc200c64eb5431fe2f8025fb861e7c7994bfe96
Kevin Blair <kblair40@gmail.com>
2022-05-03 13:05:34 -0500

    Add functionality for controlling slick by adding keyup/down listeners

--- src/components/GalleryImageCarousel/index.js
+++ src/components/GalleryImageCarousel/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import { Box, IconButton, Image, Flex } from "@chakra-ui/react";
 import { BiLeftArrowAlt, BiRightArrowAlt } from "react-icons/bi";
 import Slider from "react-slick";
@@ -6,6 +6,38 @@ import Slider from "react-slick";
 export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
   // reference variable to change the state of custom buttons
   const [slider, setSlider] = useState(null);
+
+  const keysDisabled = useRef(false);
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyPress);
+    window.addEventListener("keyup", enableKeyPress);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+      window.removeEventListener("keyup", enableKeyPress);
+    };
+  }, [slider]);
+
+  const enableKeyPress = () => {
+    if (keysDisabled.current) {
+      keysDisabled.current = false;
+    }
+  };
+
+  const handleKeyPress = (e) => {
+    if (keysDisabled.current) {
+      return;
+    }
+
+    if (e.key === "ArrowRight") {
+      slider.slickNext();
+    } else if (e.key === "ArrowLeft") {
+      slider.slickPrev();
+    }
+
+    keysDisabled.current = true;
+  };
 
   // Slider settings
   const settings = {
@@ -56,11 +88,16 @@ export const GalleryImageCarousel = ({ imagesArray, startingSlideIdx }) => {
 
       <Slider ref={(slider) => setSlider(slider)} {...settings}>
         {imagesArray.map((url, index) => (
-          <Flex justifyContent="center" w="100%" alignItems="center" h="100%">
+          <Flex
+            justifyContent="center"
+            w="100%"
+            alignItems="center"
+            h="100%"
+            key={index}
+          >
             <Image
               mx="auto"
               src={url}
-              key={index}
               maxH="80vh"
               maxW="100%"
               borderRadius="4px"
