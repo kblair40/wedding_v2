fa635330ac85e61198a2fae79ddfa2f8071070f8
Kevin Blair <kblair40@gmail.com>
2022-05-03 08:28:39 -0500

    add api key to tilelayer request from stadia

--- src/components/Map/MapContents.js
+++ src/components/Map/MapContents.js
@@ -5,11 +5,13 @@ import Markers from "./Markers";
 
 const MapContents = () => {
   const map = useMap();
+  const LEAFLET_API_KEY = process.env.REACT_APP_LEAFLET_API_KEY;
+  console.log({ LEAFLET_API_KEY });
   return (
     <React.Fragment>
       <TileLayer
         attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        url={`https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png?api_key=${LEAFLET_API_KEY}`}
       />
       {/* <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
