e93addff9ce2ebee01ec558f1dc77090571ebc7c
Kevin Blair <kblair40@gmail.com>
2022-05-09 19:12:06 -0500

    Add matching icons for each activity type

--- src/components/Map/Markers.js
+++ src/components/Map/Markers.js
@@ -3,29 +3,40 @@ import { Marker, Popup } from "react-leaflet";
 import { Text } from "@chakra-ui/react";
 import venue_marker from "assets/icons/venue_marker.png";
 import new_mickey from "assets/icons/mickey_new.svg";
-import airport from "assets/icons/airport.svg";
+// import airport from "assets/icons/airport.svg";
 import L from "leaflet";
 import hotel_icon from "assets/icons/hotel_icon.jpg";
+
+import hotel from "assets/images/markers/hotel.png";
+import park from "assets/images/markers/park.png";
+import restaurant from "assets/images/markers/restaurant.png";
+import drink from "assets/images/markers/drink.png";
+import airport from "assets/images/markers/airport.png";
 
 const Markers = () => {
   const planeIcon = L.icon({
     iconUrl: airport,
-    iconSize: [36, 36],
+    iconSize: [28, 36],
     popupAnchor: [0, 0],
   });
   const hotelIcon = L.icon({
-    iconUrl: hotel_icon,
-    iconSize: [36, 36],
+    iconUrl: hotel,
+    iconSize: [28, 36],
     popupAnchor: [0, 0],
   });
   const venueIcon = L.icon({
     iconUrl: venue_marker,
-    iconSize: [36, 36],
+    iconSize: [28, 36],
     popupAnchor: [0, 0],
   });
   const mickeyIcon = L.icon({
     iconUrl: new_mickey,
     iconSize: [64, 64],
+    popupAnchor: [0, 0],
+  });
+  const restaurantIcon = L.icon({
+    iconUrl: restaurant,
+    iconSize: [28, 36],
     popupAnchor: [0, 0],
   });
 
@@ -57,11 +68,21 @@ const Markers = () => {
     restaurants: [
       {
         // icon: restaurantIcon,
+        icon: restaurantIcon,
         position: [28.597306662119003, -81.36607228885731],
         popupText: "Hillstone Restaurant",
         websiteURL: "http://hillstonerestaurant.com/locations/winterpark/",
         directionsURL:
           "https://www.google.com/maps/dir//Hillstone+Restaurant,+215+S+Orlando+Ave,+Winter+Park,+FL+32789/@28.5970853,-81.3682288,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x88e7706dedb6b565:0x1e77ea39f3977eff!2m2!1d-81.3660401!2d28.5970806!3e0",
+      },
+      {
+        // icon: restaurantIcon,
+        icon: restaurantIcon,
+        position: [28.597753121736748, -81.3506015084386],
+        popupText: "Bosphorous Turkish Cuisine",
+        websiteURL: "http://www.bosphorousrestaurant.com/",
+        directionsURL:
+          "https://www.google.com/maps/dir//Bosphorous+Turkish+Cuisine,+108+S+Park+Ave,+Winter+Park,+FL+32789/@28.5976448,-81.3531228,17z/data=!4m9!4m8!1m0!1m5!1m1!1s0x88e7701078a3f7ed:0x11368fae0664d1a2!2m2!1d-81.3509319!2d28.5976419!3e0",
       },
     ],
   };
@@ -75,12 +96,16 @@ const Markers = () => {
       />
 
       {markers.airports.map((marker, i) => (
-        <CustomMarker {...marker} />
+        <CustomMarker {...marker} key={i} />
       ))}
 
       {markers.hotels.map((marker, i) => (
-        <CustomMarker {...marker} />
+        <CustomMarker {...marker} key={i} />
       ))}
+
+      {markers.restaurants.map((marker, i) => {
+        <CustomMarker {...marker} key={i} />;
+      })}
 
       <Marker
         icon={mickeyIcon}
@@ -96,7 +121,13 @@ const Markers = () => {
 
 export default Markers;
 
-const CustomMarker = ({ position, icon, popupText }) => {
+const CustomMarker = ({
+  position,
+  icon,
+  popupText,
+  websiteURL,
+  directionsURL,
+}) => {
   return (
     <Marker icon={icon} position={position}>
       <Popup>
