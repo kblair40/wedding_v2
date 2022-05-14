96fff1c520cfa476ca69cf2ab8fc53a20dcfd783
Kevin Blair <kblair40@gmail.com>
2022-05-14 10:12:12 -0500

    Add link to airbnb

--- src/components/Accommodations/index.js
+++ src/components/Accommodations/index.js
@@ -9,6 +9,7 @@ import {
   Stack,
   Button,
   Image,
+  Link,
 } from "@chakra-ui/react";
 
 import OverlayCard from "components/containers/OverlayCard";
@@ -22,14 +23,33 @@ const Accommodations = () => {
         accommodations
       </Heading>
 
-      <Text mt="16px">
-        Voluptate adipisicing quis velit consequat non do consectetur est ipsum
-        laboris. Adipisicing tempor culpa cillum cillum eu commodo nulla officia
-        do aute. Nulla laborum sunt fugiat consectetur nulla est Lorem.
-        Excepteur ipsum dolore velit incididunt commodo sint pariatur ex
-        proident tempo.
-      </Text>
-
+      <Box d="inline" mt="16px">
+        <Text d="inline">
+          The two closest hotels to the venue are The Hilton Garden Inn and the
+          Alfond Inn. We recommend also looking for&nbsp;
+        </Text>
+        <Button
+          d="inline"
+          variant="link"
+          color="#000"
+          // mx="3px"
+        >
+          <Link
+            href="https://www.airbnb.com/s/Winter-Park--Florida--United-States/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_lengths%5B%5D=one_week&query=Winter%20Park%2C%20Florida%2C%20United%20States&place_id=ChIJxSmm2xtw54gRIO71LJWCP8Y&date_picker_type=calendar&checkin=2023-01-20&checkout=2023-01-22&source=structured_search_input_header&search_type=autocomplete_click"
+            isExternal
+          >
+            airbnb’s
+          </Link>
+        </Button>
+        <Text d="inline">
+          &nbsp;in the Winter Park area if a hotel isn’t the right fit for you!
+        </Text>
+        {/* <Text mt="16px">
+        The two closest hotels to the venue are The Hilton Garden Inn and the
+        Alfond Inn. We recommend also looking for airbnb’s in the Winter Park
+        area if a hotel isn’t the right fit for you!
+      </Text> */}
+      </Box>
       <Flex mt="16px" justify="space-between" mb="32px">
         <OverlayCard
           imageURL={alfond}
