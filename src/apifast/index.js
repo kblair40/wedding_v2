df8fbb73f7275dd9827ca37598c32158102f6b15
Kevin Blair <kblair40@gmail.com>
2022-05-26 08:37:24 -0500

    Update baseURL to ec2 instance address

--- src/apifast/index.js
+++ src/apifast/index.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
+// https://www.youtube.com/watch?v=SgSnz7kW-Ko
 const instance = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: "http://54.242.49.140/",
+  // baseURL: "http://localhost:8000/",
   // headers: {
 
   // }
