b79c25f7e37722cd0ef8509a58d4a35fe7120a05
Kevin Blair <kblair40@gmail.com>
2022-05-26 12:43:03 -0500

    change http to https for testing

--- src/apifast/index.js
+++ src/apifast/index.js
@@ -2,11 +2,7 @@ import axios from "axios";
 
 // https://www.youtube.com/watch?v=SgSnz7kW-Ko
 const instance = axios.create({
-  baseURL: "http://54.242.49.140/",
-  // baseURL: "http://localhost:8000/",
-  // headers: {
-
-  // }
+  baseURL: "https://54.242.49.140/",
 });
 
 export default instance;
