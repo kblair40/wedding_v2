f8fb44d7ba05c4b3440b00b064061bfc76020d1a
Kevin Blair <kblair40@gmail.com>
2022-05-28 09:09:35 -0500

    update base url to use deployed api url

--- src/apimongo/index.js
+++ src/apimongo/index.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000/",
+  // baseURL: "http://localhost:3000/",
+  baseURL: "https://express-wedding-api.herokuapp.com/",
 });
 
 export default instance;
