31300b32574fae9710ed0d8237e54645146c4e0b
Kevin Blair <kblair40@gmail.com>
2022-09-02 15:10:22 -0500

    Add self-hosted version of all fonts

--- src/index.js
+++ src/index.js
@@ -4,6 +4,19 @@ import App from "./App";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
 
+// fonts
+import "@fontsource/cabin/400.css";
+import "@fontsource/cabin/400-italic.css";
+import "@fontsource/cabin/500.css";
+import "@fontsource/cabin/500-italic.css";
+import "@fontsource/cabin/600.css";
+import "@fontsource/cabin/700.css";
+
+import "@fontsource/cormorant-garamond/400.css";
+import "@fontsource/cormorant-garamond/500.css";
+import "@fontsource/cormorant-garamond/600.css";
+import "@fontsource/cormorant-garamond/700.css";
+
 import "./index.css";
 import theme from "utils/theme";
 
