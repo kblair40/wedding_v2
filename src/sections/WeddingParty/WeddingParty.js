de94552589bee3e2886e807fd5c80b180c332698
Kevin Blair <kblair40@gmail.com>
2022-09-02 06:37:24 -0500

    Replace more large images with smaller versions

--- src/sections/WeddingParty/WeddingParty.js
+++ src/sections/WeddingParty/WeddingParty.js
@@ -10,7 +10,7 @@ import SectionLabel from "components/SectionLabel";
 import Us from "components/Us";
 
 import ringBearers from "assets/images/wedding_party/augustfrederick.jpg";
-import sarlota from "assets/images/wedding_party/sarlota.webp";
+import sarlota from "assets/images/wedding_party/sarlota.jpg"; // good
 import andras from "assets/images/wedding_party/andras.webp";
 import cameron from "assets/images/wedding_party/cameron.webp";
 import ted2 from "assets/images/wedding_party/ted2.webp";
@@ -19,7 +19,7 @@ import caleb3 from "assets/images/wedding_party/caleb3.webp";
 import andrew2 from "assets/images/wedding_party/andrew2.webp";
 import karissa from "assets/images/wedding_party/karissa.webp";
 import cassidy from "assets/images/wedding_party/cassidy.webp";
-import erin2 from "assets/images/wedding_party/erin2.webp";
+import erin2 from "assets/images/wedding_party/erin2.jpg"; // good
 import kelly2 from "assets/images/wedding_party/kelly2.webp";
 
 const WeddingParty = ({ setInView, scrollPosition }) => {
