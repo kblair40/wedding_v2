f132f41b6c69af06de53bfe8e521419ecf1b2d41
Kevin Blair <kblair40@gmail.com>
2022-09-04 08:27:48 -0500

    Remove render delay for all content below main

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useRef, useState } from "react";
 import { Box, useBreakpointValue } from "@chakra-ui/react";
 
+// import RSVP from "sections/RSVP";
 import MainBackground from "components/MainBackground";
 import Travel from "sections/Travel";
 import Activities from "sections/Activities";
-// import RSVP from "sections/RSVP";
 import TempRSVPSection from "sections/RSVP/TempRSVPSection";
 import WeddingParty from "sections/WeddingParty";
 import Gallery from "sections/Gallery";
 import WhenAndWhere from "sections/WhenAndWhere";
 import Registry from "sections/Registry";
+
+// const Travel = React.lazy(() => import("sections/Travel"));
 
 const Main = ({
   section,
@@ -69,41 +71,41 @@ const Main = ({
         />
       </Box>
 
-      {imgLoaded && (
-        <React.Fragment>
-          <WhenAndWhere />
+      {/* {imgLoaded && ( */}
+      <React.Fragment>
+        <WhenAndWhere />
 
-          <Box {...boxStyle} ref={rsvpRef}>
-            <TempRSVPSection
-              setInView={() => handleChangeSectionInView("rsvp")}
-            />
-          </Box>
+        <Box {...boxStyle} ref={rsvpRef}>
+          <TempRSVPSection
+            setInView={() => handleChangeSectionInView("rsvp")}
+          />
+        </Box>
 
-          <Box {...boxStyle} ref={travelRef}>
-            <Travel setInView={() => handleChangeSectionInView("travel")} />
-          </Box>
+        <Box {...boxStyle} ref={travelRef}>
+          <Travel setInView={() => handleChangeSectionInView("travel")} />
+        </Box>
 
-          <Box {...boxStyle} ref={activitiesRef}>
-            <Activities
-              setInView={() => handleChangeSectionInView("activities")}
-            />
-          </Box>
+        <Box {...boxStyle} ref={activitiesRef}>
+          <Activities
+            setInView={() => handleChangeSectionInView("activities")}
+          />
+        </Box>
 
-          <Box {...boxStyle} ref={registryRef}>
-            <Registry setInView={() => handleChangeSectionInView("registry")} />
-          </Box>
+        <Box {...boxStyle} ref={registryRef}>
+          <Registry setInView={() => handleChangeSectionInView("registry")} />
+        </Box>
 
-          <Box {...boxStyle} ref={weddingPartyRef}>
-            <WeddingParty
-              setInView={() => handleChangeSectionInView("weddingParty")}
-            />
-          </Box>
+        <Box {...boxStyle} ref={weddingPartyRef}>
+          <WeddingParty
+            setInView={() => handleChangeSectionInView("weddingParty")}
+          />
+        </Box>
 
-          <Box {...boxStyle} ref={galleryRef}>
-            <Gallery setInView={() => handleChangeSectionInView("gallery")} />
-          </Box>
-        </React.Fragment>
-      )}
+        <Box {...boxStyle} ref={galleryRef}>
+          <Gallery setInView={() => handleChangeSectionInView("gallery")} />
+        </Box>
+      </React.Fragment>
+      {/* )} */}
     </Box>
   );
 };
