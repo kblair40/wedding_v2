35122d8bfffde97a37c54cc5cbc1704871e1f985
Kevin Blair <kblair40@gmail.com>
2022-09-05 09:00:07 -0500

    Add React.lazy to all font imports not needed on main page

--- src/index.js
+++ src/index.js
@@ -7,27 +7,27 @@ import { BrowserRouter } from "react-router-dom";
 
 // fonts
 import "@fontsource/cabin/400.css"; // need on initial load
-import "@fontsource/cabin/400-italic.css";
+// import "@fontsource/cabin/400-italic.css";
 import "@fontsource/cabin/500.css"; // need on initial load
-import "@fontsource/cabin/500-italic.css";
-import "@fontsource/cabin/600.css";
-import "@fontsource/cabin/700.css";
+// import "@fontsource/cabin/500-italic.css";
+// import "@fontsource/cabin/600.css";
+// import "@fontsource/cabin/700.css";
 
-import "@fontsource/cormorant-garamond/400.css";
-import "@fontsource/cormorant-garamond/500.css";
-import "@fontsource/cormorant-garamond/600.css";
+// import "@fontsource/cormorant-garamond/400.css";
+// import "@fontsource/cormorant-garamond/500.css";
+// import "@fontsource/cormorant-garamond/600.css";
 import "@fontsource/cormorant-garamond/700.css"; // need on initial load
 
 import "./index.css";
 import theme from "utils/theme";
 
-// loadable(() => import("@fontsource/cabin/400-italic.css"));
-// loadable(() => import("@fontsource/cabin/500-italic.css"));
-// loadable(() => import("@fontsource/cabin/600.css"));
-// loadable(() => import("@fontsource/cabin/700.css"));
-// loadable(() => import("@fontsource/cormorant-garamond/400.css"));
-// loadable(() => import("@fontsource/cormorant-garamond/500.css"));
-// loadable(() => import("@fontsource/cormorant-garamond/600.css"));
+React.lazy(() => import("@fontsource/cabin/400-italic.css"));
+React.lazy(() => import("@fontsource/cabin/500-italic.css"));
+React.lazy(() => import("@fontsource/cabin/600.css"));
+React.lazy(() => import("@fontsource/cabin/700.css"));
+React.lazy(() => import("@fontsource/cormorant-garamond/400.css"));
+React.lazy(() => import("@fontsource/cormorant-garamond/500.css"));
+React.lazy(() => import("@fontsource/cormorant-garamond/600.css"));
 
 ReactDOM.render(
   <ChakraProvider theme={theme}>
