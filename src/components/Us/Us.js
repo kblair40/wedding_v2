3278cdcc673916b979ab8d30ad54220be59b9207
Kevin Blair <kblair40@gmail.com>
2022-10-18 08:02:49 -0500

    Refactor animation to remove need for relative positioning

--- src/components/Us/Us.js
+++ src/components/Us/Us.js
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from "react";
-import { Flex, Box, Icon, useBreakpointValue } from "@chakra-ui/react";
+import React, { useEffect, useState, useRef } from "react";
+import { Flex, Box, useBreakpointValue } from "@chakra-ui/react";
 import { useInView } from "react-intersection-observer";
 import { gsap } from "gsap";
 
@@ -20,6 +20,9 @@ const Us = () => {
     triggerOnce: true,
   };
 
+  const brideRef = useRef();
+  const groomRef = useRef();
+
   const [inViewRef, inView] = useInView(options);
 
   useEffect(() => {
@@ -31,14 +34,22 @@ const Us = () => {
   }, [inView]);
 
   const slideTogether = () => {
-    gsap.to(".bride", { duration: 1, x: transitionAmount });
-    gsap.to(".groom", { duration: 1, x: -transitionAmount });
+    // gsap.to(".bride", { duration: 1, x: transitionAmount });
+    // gsap.to(".groom", { duration: 1, x: -transitionAmount });
+    gsap.to(brideRef.current, {
+      duration: 1,
+      x: transitionAmount,
+      // onComplete: () => (brideRef.current.style.position = "static"),
+    });
+    gsap.to(groomRef.current, { duration: 1, x: -transitionAmount });
     gsap.to(".heart", { duration: 2, opacity: 1 });
   };
 
   const slideApart = () => {
-    gsap.to(".bride", { duration: 1, x: 0 });
-    gsap.to(".groom", { duration: 1, x: 0 });
+    // gsap.to(".bride", { duration: 1, x: 0 });
+    // gsap.to(".groom", { duration: 1, x: 0 });
+    gsap.to(brideRef.current, { duration: 1, x: 0 });
+    gsap.to(groomRef.current, { duration: 1, x: 0 });
     gsap.to(".heart", { duration: 1, opacity: 0 });
   };
 
@@ -54,7 +65,7 @@ const Us = () => {
     }
   }, [inView]);
 
-  let shift = useBreakpointValue({ base: 0, sm: "60px" });
+  // let shift = useBreakpointValue({ base: 0, sm: "60px" });
   let kevImg = useBreakpointValue({ base: kev, sm: kevface });
   let shanImg = useBreakpointValue({ base: shan, sm: shanface });
   let imgSize = useBreakpointValue({ base: "250px", sm: "180px" });
@@ -68,34 +79,84 @@ const Us = () => {
       justifyContent={{ sm: "space-between" }}
       alignItems="center"
       ref={inViewRef}
+      border="1px solid lightgreen"
+      height={{ base: "auto", sm: "320px" }}
     >
-      <Box className="us bride" position="relative" top={shift}>
-        <Partier
-          name="shannon dunne"
-          role="BRIDE"
-          imgURL={shanImg}
-          position="center 20%"
-        />
-      </Box>
-      <Box className="heart" position="relative" bottom="16px" opacity={0}>
-        {/* <Icon as={FaRegHeart} boxSize="40px" fill="error.400" /> */}
+      <Flex h="100%" align="end">
+        <Box ref={brideRef} className="us bride">
+          <Partier
+            name="shannon dunne"
+            role="BRIDE"
+            imgURL={shanImg}
+            position="center 20%"
+          />
+        </Box>
+      </Flex>
+
+      <Box
+        className="heart"
+        opacity={0}
+        //
+      >
         <HeartIcon />
       </Box>
-      <Box
-        className="us groom"
-        position="relative"
-        bottom={shift}
-        sx={{
-          img: {
-            height: imgSize,
-            width: imgSize,
-          },
-        }}
-      >
-        <Partier name="kevin blair" role="GROOM" imgURL={kevImg} />
-      </Box>
+
+      <Flex h="100%">
+        <Box
+          ref={groomRef}
+          className="us groom"
+          sx={{
+            img: {
+              height: imgSize,
+              width: imgSize,
+            },
+          }}
+        >
+          <Partier name="kevin blair" role="GROOM" imgURL={kevImg} />
+        </Box>
+      </Flex>
     </Flex>
   );
 };
 
 export default Us;
+
+// return (
+//   <Flex
+//     mb={{ base: "32px", sm: "80px" }}
+//     mt={{ base: 0, sm: "48px" }}
+//     w="100%"
+//     flexDirection={{ base: "column", sm: "row" }}
+//     justifyContent={{ sm: "space-between" }}
+//     alignItems="center"
+//     ref={inViewRef}
+//   >
+//     <Box ref={brideRef} className="us bride" position="relative" top={shift}>
+//       <Partier
+//         name="shannon dunne"
+//         role="BRIDE"
+//         imgURL={shanImg}
+//         position="center 20%"
+//       />
+//     </Box>
+
+//     <Box className="heart" position="relative" bottom="16px" opacity={0}>
+//       <HeartIcon />
+//     </Box>
+
+//     <Box
+//       ref={groomRef}
+//       className="us groom"
+//       position="relative"
+//       bottom={shift}
+//       sx={{
+//         img: {
+//           height: imgSize,
+//           width: imgSize,
+//         },
+//       }}
+//     >
+//       <Partier name="kevin blair" role="GROOM" imgURL={kevImg} />
+//     </Box>
+//   </Flex>
+// );
